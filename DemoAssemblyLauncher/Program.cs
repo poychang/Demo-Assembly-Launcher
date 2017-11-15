19a3704a223cf8595d19066b9253a1d48e8d4659
Poy Chang <poy.chang@seec.com.tw>
2017-11-15 12:26:20 +0800

    建立 DemoAssemblyLauncher 專案，測試 AssemblyLauncher 的執行方法

---
+++ DemoAssemblyLauncher/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using DynamicExecuteAssembly;
+using Newtonsoft.Json;
+
+namespace DemoAssemblyLauncher
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var assemblyLauncher = new AssemblyLauncher();
+            var result1 = assemblyLauncher.Execute<BizLagicObjcet, object>(new BizLagicObjcet(), "GetString", "");
+            Console.WriteLine(JsonConvert.SerializeObject(result1));
+
+            var result2 = assemblyLauncher.Execute<BizLagicObjcet, object>(new BizLagicObjcet(), "GetStringWithParamter", "'Poy'");
+            Console.WriteLine(JsonConvert.SerializeObject(result2));
+
+            try
+            {
+                var result3 = assemblyLauncher.Execute<BizLagicObjcet, object>(new BizLagicObjcet(), "GetStringWith2Paramter", "'Poy', 'Chang'");
+                Console.WriteLine(JsonConvert.SerializeObject(result3));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
