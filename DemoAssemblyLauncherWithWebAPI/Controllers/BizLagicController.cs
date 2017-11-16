fce366c177ec8dfc5278e3fb4938c33d25e04008
Poy Chang <poy.chang@seec.com.tw>
2017-11-16 12:07:17 +0800

    實作搭配 WebAPI 的 DEMO 專案

---
+++ DemoAssemblyLauncherWithWebAPI/Controllers/BizLagicController.cs
@@ -0,0 +1,29 @@
+﻿using DemoBizLogic;
+using DynamicExecuteAssembly;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace DemoAssemblyLauncherWithWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class BizLagicController : Controller
+    {
+        public AssemblyLauncher AssemblyLauncher = new AssemblyLauncher();
+
+        // GET api/BizLagic/GetString
+        [HttpGet("{method}")]
+        public IActionResult Get(string method)
+        {
+            var result = AssemblyLauncher.Execute<BizLagicObjcet, object>(new BizLagicObjcet(), method, "");
+            return new JsonResult(result);
+        }
+
+        //POST api/BizLagic/GetStringWithParamter
+        [HttpPost("{method}")]
+        public IActionResult Post(string method, [FromBody]BizLagicModel parameter)
+        {
+            var result = AssemblyLauncher.Execute<BizLagicObjcet, object>(new BizLagicObjcet(), method, JsonConvert.SerializeObject(parameter));
+            return new JsonResult(result);
+        }
+    }
+}
