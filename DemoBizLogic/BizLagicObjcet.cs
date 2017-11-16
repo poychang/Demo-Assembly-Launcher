e5ba58f90e9fe5e81c15b2e1803c60410f7ca3c3
Poy Chang <poy.chang@seec.com.tw>
2017-11-16 12:06:15 +0800

    抽離 BizLogicObject 至 DemoBizLogic 專案中

---
+++ DemoBizLogic/BizLagicObjcet.cs
@@ -0,0 +1,42 @@
+﻿using DynamicExecuteAssembly;
+
+namespace DemoBizLogic
+{
+    public class BizLagicObjcet
+    {
+        public IExecuteResult<object> GetString()
+        {
+            return new DefaultExecuteResult<object>()
+            {
+                Data = "This is string from BizLogic'a GetString",
+                IsSuccess = true,
+                Message = "Good Job"
+            };
+        }
+
+        public IExecuteResult<object> GetStringWithParamter(BizLagicModel data)
+        {
+            return new DefaultExecuteResult<object>()
+            {
+                Data = $"This is string from BizLogic'a GetString with {data.Name}",
+                IsSuccess = true,
+                Message = "Good Job"
+            };
+        }
+
+        public IExecuteResult<object> GetStringWith2Paramter(BizLagicModel data1, BizLagicModel data2)
+        {
+            return new DefaultExecuteResult<object>()
+            {
+                Data = $"This is string from BizLogic'a GetString with {data1.Name} and {data2.Name}",
+                IsSuccess = true,
+                Message = "Good Job"
+            };
+        }
+    }
+
+    public class BizLagicModel
+    {
+        public string Name { get; set; }
+    }
+}
